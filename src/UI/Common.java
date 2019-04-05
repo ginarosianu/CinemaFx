18de0b290d0596cc0ee6521858a45191fb12ea2b
gina <gina.rosianu@gmail.com>
2019-04-06 01:20:02 +0300

    cinemaFxNou

---
+++ src/UI/Common.java
@@ -0,0 +1,13 @@
+package UI;
+
+import javafx.scene.control.Alert;
+
+public class Common {
+    static void showValidationError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error!");
+        alert.setHeaderText("Error:");
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
